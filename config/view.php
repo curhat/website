662d6e7b01af4e4d0f913bf8cfe8b37ee32025fe
Ary Wibowo <nucreativa@gmail.com>
2018-10-14 22:05:50 +0700

    Configuration for Google App Engine

--- config/view.php
+++ config/view.php
@@ -17,17 +17,18 @@ return [
         resource_path('views'),
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Compiled View Path
-    |--------------------------------------------------------------------------
-    |
-    | This option determines where all the compiled Blade templates will be
-    | stored for your application. Typically, this is within the storage
-    | directory. However, as usual, you are free to change this value.
-    |
-    */
-
-    'compiled' => realpath(storage_path('framework/views')),
+	# [START google-app-engine-deployment]
+	/*
+	|--------------------------------------------------------------------------
+	| Compiled View Path
+	|--------------------------------------------------------------------------
+	|
+	| This option determines where all the compiled Blade templates will be
+	| stored for your application. Typically, this is within the storage
+	| directory. However, as usual, you are free to change this value.
+	|
+	*/
+    'compiled' => storage_path(),
+	# [END google-app-engine-deployment]
 
 ];
